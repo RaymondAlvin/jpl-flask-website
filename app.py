d48e583818cd0edc7a4d6e40861214bc14759e69
RaymondAlvin <108366163+RaymondAlvin@users.noreply.github.com>
2023-06-21 16:22:44 +0000

    Added home page

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Pasadena, California',
+    'salary': '$100,000'
+  },
+  {
+    'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Pasadena, California',
+  'salary': '$150,000'
+  },
+  {
+  'id': 3,
+  'title': 'Frontend Engineer',
+  'location': 'Remote',
+  },
+  {
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'San Fransisco, California',
+  'salary': '$175,000'
+  }
+  
+
+
+
+]
+
 @app.route("/")
 def hello_world():
-  return "Hello World!"
+  return render_template('home.html',
+                        jobs=JOBS,
+                        company_name='JPL')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
